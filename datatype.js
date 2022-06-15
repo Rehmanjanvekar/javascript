fa7ce4148b8bde8cfb22d1f247455350f8f4a977
Rehmanjanvekar <106329889+Rehmanjanvekar@users.noreply.github.com>
2022-06-15 14:21:36 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,19 @@
+var genral;
+console.log(genral);
+genral=6;
+console.log(typeof genral);
+genral=rehman;
+console.log(typeof genral);
+genral="hello";
+console.log(typeof genral);
+genral=true;
+
+console.log(20+30+10)
+console.log(20-10-5)
+console.log("hello"+"this"+"rehman")
+
+var num1=prompt("enter some value");
+console.log(typeof num1)
+
+console.log(parsenInt(num1)+5);
+alert("hello this is an alert");
